544ee57021093cfbb52d5e73c4affd6015a77d95
RAMYA-1994 <ramyarrr2016@gmail.com>
2024-12-12 11:35:58 +0530

    update

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -41,6 +41,19 @@ const About = () => {
                 </p>
               </span>
             </div>
+            <div className="flex gap-3 py-4">
+              <IoArrowForward size={30} className="mt-1" />
+
+              <span className="w-96">
+                <h1 className="text-xl md:text-2xl font-semibold leading-normal">
+                  Full stack developer
+                </h1>
+                <p className="text-sm md:text-md leading-tight">
+                A database is an organized collection of data that enables efficient storage,
+                 retrieval, and management.
+                </p>
+              </span>
+            </div>
           </ul>
         </div>
       </div>
